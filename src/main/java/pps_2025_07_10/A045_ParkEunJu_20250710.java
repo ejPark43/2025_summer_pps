218e1609161763883c3a4bbaad41f7bef5d7f2d8
ejPark43 <pej22200310@handong.ac.kr>
2025-07-10 22:43:58 +0900

    20250710 문제풀이 A033,A034,A035,A045,A046,A047

---
+++ src/main/java/pps_2025_07_10/A045_ParkEunJu_20250710.java
@@ -0,0 +1,40 @@
+package pps_2025_07_10;
+
+import java.util.Scanner;
+
+// A045 단어 공부:  알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성한다.
+
+public class A045_ParkEunJu_20250710 {
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+        String word = s.next();
+
+        word = word.toUpperCase();
+
+        String[] word_chars = word.split("");
+        int[] alphabetCount = new int[26];
+
+        for(int i=0; i<word.length(); i++){
+            alphabetCount[word_chars[i].charAt(0) - 65] ++;
+
+        }
+
+        int max=-100;
+        char result = '?'; // 여러 알파벳이 동일하게 같이 사용되었다면 물음표
+        for(int i=0; i<26; i++){
+
+            if(max==alphabetCount[i])   {
+                result = '?';
+
+            }
+            if(alphabetCount[i]>max) {
+                max = alphabetCount[i];
+                result = (char)(i+'A');
+            }
+        }
+
+        System.out.println(result);
+    }
+
+}
---
+++ src/main/java/pps_2025_07_10/A047_ParkEunJu_20250710.java
@@ -0,0 +1,25 @@
+package pps_2025_07_10;
+
+import java.util.Scanner;
+
+// A047 열개씩 끊어 출력하기:  입력으로 주어진 단어를 열 개씩 끊어서 한 줄에 하나씩 출력한다.
+
+public class A047_ParkEunJu_20250710 {
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+
+        String word = s.next();
+        int length = word.length();
+        for(int i=length; i>=1; i-=10){
+            if(word.length()<10) System.out.println(word);
+            else{
+                System.out.println(word.substring(0, 10));
+                word = word.substring(10);
+            }
+        }
+
+
+    }
+
+}
